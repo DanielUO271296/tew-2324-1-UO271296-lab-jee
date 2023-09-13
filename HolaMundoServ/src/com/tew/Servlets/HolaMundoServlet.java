cc17ae5b5c6cbcde76dd818664c74e5b0eb818ae
uo271296 <uo271296@GI-AN-B6_16.aulasuo.uniovi.es>
2023-09-13 17:54:27 +0200

    primera carga

---
+++ HolaMundoServ/src/com/tew/Servlets/HolaMundoServlet.java
@@ -0,0 +1,56 @@
+package com.tew.Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HolaMundoServlet
+ */
+@WebServlet("/HolaMundoServlet")
+public class HolaMundoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HolaMundoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+			 response.setCharacterEncoding("UTF-8");
+			 response.setContentType("text/html");
+			 
+			 //se puede entrar desde la web usando esta url http://localhost:8080/HolaMundoServ/HolaMundoCordial
+			 //la barra del buscador hace peticiones get
+			 PrintWriter out = response.getWriter();
+			 out.println("<HTML>");
+			 out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+			 out.println("<BODY>");
+			 out.println("Bienvenido a mi primera página web!");
+			 out.println("</BODY></HTML>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
